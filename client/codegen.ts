7f14011c3322d99b5bb715c8d7e6c16c3a5f4809
Anton Maiseyeu <anton.maiseyeu@gmail.com>
2026-03-12 15:13:15 -0400

    feat: enable codegen client preset configuration

--- client/codegen.ts
+++ client/codegen.ts
@@ -3,19 +3,13 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: '../server/src/schema/schema.graphql',
-  documents: ['src/**/*.graphql'],
+  documents: ['src/**/*.{ts,tsx}'],
   ignoreNoDocuments: true,
   generates: {
-    'src/generated/graphql.ts': {
-      plugins: [
-        'typescript',
-        'typescript-operations',
-        'typescript-react-apollo',
-      ],
+    'src/generated/': {
+      preset: 'client',
       config: {
-        withHooks: true,
-        withComponent: false,
-        withHOC: false,
+        useTypeImports: true,
         nonOptionalTypename: true,
         skipTypename: false,
       },
