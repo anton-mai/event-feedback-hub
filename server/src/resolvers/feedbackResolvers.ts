4f66897f68c71fa4f774c5dabff47cb5c33fdfc3
Anton Maiseyeu <anton.maiseyeu@gmail.com>
2026-03-18 21:55:43 -0400

    refactor: moved getEventById logic to eventService

--- server/src/resolvers/feedbackResolvers.ts
+++ server/src/resolvers/feedbackResolvers.ts
@@ -1,12 +1,14 @@
 import { getFeedbackCreatedChannel, pubsub } from '../pubsub.js';
-import { createFeedback, getFeedback } from '../services/index.js';
-import { getEventItemById } from '../store/eventStore.js';
+import {
+  createFeedback,
+  getEventById,
+  getFeedback,
+} from '../services/index.js';
 import {
   TCreateFeedbackInput,
   TFeedbackItem,
   TGetFeedbackParams,
 } from '../types/index.js';
-import { createNotFoundError } from '../utils/errors.js';
 
 const feedbackQueryResolvers = {
   feedback: (_: unknown, args: TGetFeedbackParams) => getFeedback(args),
@@ -27,15 +29,7 @@ const feedbackSubscriptionResolvers = {
 };
 
 const feedbackTypeResolvers = {
-  event: (parent: TFeedbackItem) => {
-    const event = getEventItemById(parent.eventId);
-
-    if (!event) {
-      throw createNotFoundError(`Event with id "${parent.eventId}" not found.`);
-    }
-
-    return event;
-  },
+  event: (parent: TFeedbackItem) => getEventById(parent.eventId),
 };
 
 export const feedbackResolvers = {
--- server/src/services/eventService/eventService.ts
+++ server/src/services/eventService/eventService.ts
@@ -1,4 +1,15 @@
-import { getEventItems } from '../../store/eventStore.js';
+import { getEventItemById, getEventItems } from '../../store/eventStore.js';
 import { TEventItem } from '../../types/index.js';
+import { createNotFoundError } from '../../utils/errors.js';
 
 export const getAllEvents = (): TEventItem[] => getEventItems();
+
+export const getEventById = (id: string): TEventItem => {
+  const event = getEventItemById(id);
+
+  if (!event) {
+    throw createNotFoundError(`Event with id "${id}" not found.`);
+  }
+
+  return event;
+};
