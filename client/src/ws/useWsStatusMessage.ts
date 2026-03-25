e7b349b549c54f5ae3a9ce75aa55079e13251d17
Anton Maiseyeu <anton.maiseyeu@gmail.com>
2026-03-24 23:08:21 -0400

    feat: add delay to show ws status message

--- client/src/ws/useWsStatusMessage.ts
+++ client/src/ws/useWsStatusMessage.ts
@@ -1,21 +1,39 @@
 import { useReactiveVar } from '@apollo/client/react';
+import { useEffect, useState } from 'react';
 import { wsStatusVar } from './wsStatus';
 
 type TUseWsStatusMessageResult = {
   message: string | null;
 };
 
+const SHOW_DELAY = 2000;
+
 export const useWsStatusMessage = (): TUseWsStatusMessageResult => {
   const status = useReactiveVar(wsStatusVar);
+  const [delayedStatus, setDelayedStatus] = useState(status);
 
-  if (status !== 'reconnecting' && status !== 'disconnected') {
-    return { message: null };
+  useEffect(() => {
+    const timer = setTimeout(
+      () => {
+        setDelayedStatus(status);
+      },
+      ['disconnected', 'reconnecting'].includes(status) ? SHOW_DELAY : 0,
+    );
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [status]);
+
+  if (delayedStatus === 'reconnecting') {
+    return { message: 'Live updates paused. Reconnecting…' };
   }
 
-  return {
-    message:
-      status === 'reconnecting'
-        ? 'Live updates paused. Reconnecting…'
-        : 'Live updates unavailable. Please check your connection.',
-  };
+  if (delayedStatus === 'disconnected') {
+    return {
+      message: 'Live updates unavailable. Please check your connection.',
+    };
+  }
+
+  return { message: null };
 };
